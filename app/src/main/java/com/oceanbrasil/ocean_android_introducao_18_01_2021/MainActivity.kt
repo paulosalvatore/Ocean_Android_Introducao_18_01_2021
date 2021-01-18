88361cd80c69019565e5c397d5d4eba8957ac639
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-18 16:32:12 -0300

    Manipulação do tvResultado via código da Activity

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_18_01_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_18_01_2021/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.oceanbrasil.ocean_android_introducao_18_01_2021
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+        tvResultado.text = "Olá, Paulo Salvatore!"
     }
 }
