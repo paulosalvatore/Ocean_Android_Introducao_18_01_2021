c082484db8dfa8e8ecd93a042cfa710a20ae7662
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-18 14:50:05 -0300

    Initial commit

---
+++ app/src/test/java/com/oceanbrasil/ocean_android_introducao_18_01_2021/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.oceanbrasil.ocean_android_introducao_18_01_2021
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
